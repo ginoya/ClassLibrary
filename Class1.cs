cdcf84e9083d467679fc20a2ae0cfaf3d4269ee8
ginoya <rishitginoya@gmail.com>
2023-11-20 10:13:23 +0530

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary
+{
+    public class Demo
+    {
+        public void message()
+        {
+            Console.WriteLine("I am great");
+        }
+
+    }
+}
---
+++ StringExtender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public static class StringExtender
+    {
+        public static string ToCurrency(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                throw new ArgumentNullException("String cannot be null or empty.");
+
+            if (str.All(char.IsDigit))
+                return "$" + str;
+
+            throw new ArgumentException("String must contain at least one digit.");
+        }
+    }
+}
